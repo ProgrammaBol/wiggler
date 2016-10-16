f3b16572507600d968e67691a0832d60d657598b
Gabriele Cerami <gcerami@rascal>
2016-10-16 02:02:46 +0200

    Per frame object code
    Object code now may include loops.
    Yield statement is now force-injected in every loop in object code

--- core/ui.py
+++ core/ui.py
@@ -73,9 +73,61 @@ class RootWindow(wx.Frame):
         self.SetSizer(sizer)
         self.Layout()
 
+
+    def find_next_line(self, index, split_raw_code):
+        if index is None:
+            return None, None
+        while index < len(split_raw_code) - 1:
+            index = index + 1
+            line = split_raw_code[index]
+            if line != '':
+                return index, line
+        return None, None
+
+
     def play(self, event):
         raw_code = self.editor.GetText()
-        indented_code = '\n'.join(map(lambda s: re.sub("^"," "*8,s), raw_code.splitlines()))
+        split_raw_code = raw_code.splitlines()
+        lines = len(split_raw_code)
+        print split_raw_code
+        index = -1
+        loops = [None]
+        indent = 0
+        index, line = self.find_next_line(index, split_raw_code)
+        while line:
+            print "line %d"
+            print line
+            m = re.match("(\s*)(for|while)", line)
+            if m:
+                print "match found on line %d" % index
+                index, line = self.find_next_line(index, split_raw_code)
+                m = re.match("(\s*)(.+)", line)
+                print "first loop line %d - %s " % (index, line)
+                loops.insert(0, len(m.groups()[0]))
+                print "loop block indentation %d" % loops[0]
+                index, line = self.find_next_line(index, split_raw_code)
+            if loops[0]:
+                if line:
+                    m = re.match("(\s*)(.*)", line)
+                    indent = len(m.groups()[0])
+                    print "indent %d" % indent
+                    next_index = index + 1
+                    if indent < loops[0]:
+                        print "dedent from %d to %d on line %d" % (loops[0], indent, index)
+                        print "injecting yield in line %d" % index
+                        # dedent, end code block
+                        split_raw_code.insert(index, ' '* loops[0] + "yield")
+                        lines += 1
+                        loops.pop(0)
+                if line is None or index == lines - 1:
+                    print "End of file after loop"
+                    print "injecting yield in line %d" % (lines -1)
+                    # dedent, end code block
+                    split_raw_code.insert(lines , ' '* loops[0] + "yield")
+                    loops.pop(0)
+            index, line = self.find_next_line(index, split_raw_code)
+
+        indented_code = '\n'.join(map(lambda s: re.sub("^"," "*8,s), split_raw_code))
         env = jinja2.Environment(loader=jinja2.FileSystemLoader("core/templates"))
         template = env.get_template("element.j2")
         element_code = template.render(class_name="example", indented_code=indented_code)
